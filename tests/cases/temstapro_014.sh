2fcde13b641ff5a79d06555f06f14bef7526dc86
Ieva Pudziuvelyte <pudziuvelyteieva@gmail.com>
2023-06-21 12:42:52 +0300

    Made printing thermophilicity interpretation only in the 40-80 thresholds range.

--- tests/cases/temstapro_014.sh
+++ tests/cases/temstapro_014.sh
@@ -4,8 +4,6 @@
 # classifier
 
 ./temstapro -f ./tests/data/long_sequence_2.fasta -e 'tests/outputs/' -d './ProtTrans/' \
-    --mean-out tests/outputs/014.tmp --thresholds-range ':(20]:'
+    --thresholds-range ':(40-80]:'
 
 rm -f tests/outputs/mean_52ae55d4fc194abf0e65abc9d740ffc7f84972ddacefb62e931131655083857e.pt
-
-rm -f tests/outputs/014.tmp
